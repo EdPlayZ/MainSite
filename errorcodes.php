b33980316f952cd759d682398ca503a32768f3bb
EdPlayZ <catcap123@gmail.com>
2021-12-09 10:50:25 +0100

    added errorcodes.php

---
+++ errorcodes.php
@@ -0,0 +1,12 @@
+<?php
+
+# Error Code 1
+$error_1 = 'Username or Password is invalid';
+
+#Error Code 2
+$error_2 = 'Username already exists';
+
+#Error Code 3
+$error_3 = 'Invalid Secret Key';
+
+?>
