57ce1bd5ebc558d1bbd5ce091d3744bf161292d8
EdPlayZ <catcap123@gmail.com>
2022-01-31 18:36:08 +0100

    Some old work

--- test.php
+++ test.php
@@ -3,20 +3,5 @@
 </html>
 
 <?php
-/*    $dbc = mysqli_connect('localhost', 'root', 'r00t', 'TestBase')
-    or die('Error connecting to MySQL server.');
-    $username='test';
 
-    $Salt = mysqli_query($dbc, "SELECT `Salt`, `Password` from `Logins` where `Username`='$username'");
-
-    $row = $Salt->fetch_assoc();
-    $salt = $row['Salt'];
-    $password = 'test';
-    $hashedpass=hash('sha256', $password.$salt);
-    
-    echo($hashedpass)
-    */
-    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    $randstr = md5(rand());
-    echo($randstr);
 ?>
